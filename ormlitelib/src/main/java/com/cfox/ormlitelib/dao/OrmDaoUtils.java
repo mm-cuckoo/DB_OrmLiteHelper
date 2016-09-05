c1a45735d0043f43220e6eb538dbfa6957103ed0
84164861@qq.com <machao200702224>
2016-09-05 10:55:48 +0800

    添加必要注释

--- ormlitelib/src/main/java/com/cfox/ormlitelib/dao/OrmDaoUtils.java
+++ ormlitelib/src/main/java/com/cfox/ormlitelib/dao/OrmDaoUtils.java
@@ -1,13 +1,10 @@
 package com.cfox.ormlitelib.dao;
 
-import android.content.ContentValues;
 
 import com.cfox.ormlitelib.helper.OrmDatabaseHelper;
 import com.j256.ormlite.dao.Dao;
-import com.j256.ormlite.dao.ObjectCache;
 import com.j256.ormlite.misc.TransactionManager;
 import com.j256.ormlite.stmt.PreparedQuery;
-import com.j256.ormlite.stmt.PreparedUpdate;
 import com.j256.ormlite.stmt.QueryBuilder;
 import com.j256.ormlite.stmt.UpdateBuilder;
 import com.j256.ormlite.stmt.Where;
@@ -44,6 +41,7 @@ public abstract class OrmDaoUtils<T> {
     protected abstract OrmDatabaseHelper getHelper();
 
 
+
     public OrmDaoUtils(Class cls) throws SQLException {
         if (mHelper == null) {
             mHelper = getHelper();
@@ -51,14 +49,34 @@ public abstract class OrmDaoUtils<T> {
         this.mDao = mHelper.getDao(cls);
     }
 
+    /**
+     * 查询，通过ID
+     * @param objId 查询数据的ID
+     * @return 返回查询 Bean 数据，如果未查询到则返回 null
+     * @throws SQLException
+     */
     public T queryById(Integer objId) throws SQLException {
         return (T) mDao.queryForId(objId);
     }
 
+    /**
+     * 查询指定 Bean 所有数据
+     * @return 返回查询 Bean 数据，如果未查询到则返回 null
+     * @throws SQLException
+     */
     public List<T> queryAll() throws SQLException {
         return mDao.queryForAll();
     }
 
+    /**
+     * 查询，条件查询
+     * <br/> 说明： 该方法要和 {@link OrmDaoUtils#query(QueryBuilder)} 方法组合使用
+     * <br/> 在查询操作中，查询的条件最后都要 通过 {@link OrmDaoUtils#query(QueryBuilder)} 方法执行查询
+     * <br/> 此方法 对应SQL ：SELECT * FROM `t_person` WHERE `id` = 2
+     * @param whereMap
+     * @return
+     * @throws SQLException
+     */
     public QueryBuilder queryBuilder(Map<String, Object> whereMap) throws SQLException {
 
         QueryBuilder<T, Integer> queryBuilder = mDao.queryBuilder();
@@ -79,6 +97,13 @@ public abstract class OrmDaoUtils<T> {
         return queryBuilder;
     }
 
+    /**
+     * 升序或降序
+     * @param orderColumn 升序、降序字段
+     * @param isASC true 升序 ，false 降序
+     * @return
+     * @throws SQLException
+     */
     public QueryBuilder queryOrderBy(String orderColumn ,boolean isASC) throws SQLException {
         return queryOrderBy(null,orderColumn,isASC);
     }
@@ -86,6 +111,15 @@ public abstract class OrmDaoUtils<T> {
     /**
      *
      * @param isASC 参数 false 表示降序，true 表示升序。
+     * @return
+     * @throws SQLException
+     */
+
+    /**
+     * 带条件 升序或降序查询
+     * @param queryBuilder 查询条件
+     * @param orderColumn 升序、降序字段
+     * @param isASC true 升序 ，false 降序
      * @return
      * @throws SQLException
      */
@@ -117,10 +151,22 @@ public abstract class OrmDaoUtils<T> {
         return mDao.create(obj);
     }
 
+    /**
+     * 批量添加数据
+     * @param objs
+     * @return
+     * @throws SQLException
+     */
     public int insert(Collection<T> objs) throws SQLException {
         return mDao.create(objs);
     }
 
+    /**
+     * 使用事务批量添加数据
+     * @param objs
+     * @return
+     * @throws SQLException
+     */
     public boolean insertInTransaction(final List<T> objs) throws SQLException {
         return baseTransaction(INSERT, objs);
     }
@@ -128,7 +174,6 @@ public abstract class OrmDaoUtils<T> {
 
     /**
      * 删除数据
-     *
      * @param obj
      * @throws SQLException
      */
@@ -136,18 +181,41 @@ public abstract class OrmDaoUtils<T> {
         mDao.delete(obj);
     }
 
+    /**
+     * 批量删除数据
+     * @param objs
+     * @return
+     * @throws SQLException
+     */
     public int delete(Collection<T> objs) throws SQLException {
         return mDao.delete(objs);
     }
 
+    /**
+     * 通过ID 删除数据
+     * @param objId
+     * @throws SQLException
+     */
     public void deleteById(Integer objId) throws SQLException {
         mDao.deleteById(objId);
     }
 
+    /**
+     * 使用事务批量删除事务（实体）
+     * @param objs
+     * @return
+     * @throws SQLException
+     */
     public boolean deleteInTransaction(final List<T> objs) throws SQLException {
         return baseTransaction(DELETE, objs);
     }
 
+    /**
+     * 使用事务批量删除事务（ID）
+     * @param objIds
+     * @return
+     * @throws SQLException
+     */
     public boolean deleteInTransactionById(final List<Integer> objIds) throws SQLException {
         Boolean b = TransactionManager.callInTransaction(mHelper.getConnectionSource(), new Callable<Boolean>() {
             @Override
@@ -166,10 +234,24 @@ public abstract class OrmDaoUtils<T> {
     }
 
 
+    /**
+     * 修改数据
+     * @param obj
+     * @return
+     * @throws SQLException
+     */
     public int update(T obj) throws SQLException {
         return mDao.update(obj);
     }
 
+    /**
+     * 条件修改数据
+     * @param whereMap 修改条件
+     * @param columnName 修改字段名
+     * @param columnValue 修改内容
+     * @return
+     * @throws SQLException
+     */
     public int update(Map<String, Object> whereMap, String columnName, Object columnValue) throws SQLException {
         Map<String, Object> updateValue = new HashMap<String, Object>();
         updateValue.put(columnName, columnValue);
@@ -178,6 +260,13 @@ public abstract class OrmDaoUtils<T> {
     }
 
 
+    /**
+     * 条件修改数据
+     * @param whereMap
+     * @param updateValue
+     * @return
+     * @throws SQLException
+     */
     public int update(Map<String, Object> whereMap, Map<String, Object> updateValue) throws SQLException {
         UpdateBuilder updateBuilder = mDao.updateBuilder();
         if (updateValue != null && !updateValue.isEmpty()) {
@@ -205,14 +294,34 @@ public abstract class OrmDaoUtils<T> {
         return mDao.update(updateBuilder.prepare());
     }
 
+    /**
+     * 创建或修改数据
+     * <br/> 如果没有数据就添加，如果数据存在就修改
+     * @param obj
+     * @return
+     * @throws SQLException
+     */
     public Dao.CreateOrUpdateStatus createOrUpdate(T obj) throws SQLException {
         return mDao.createOrUpdate(obj);
     }
 
+    /**
+     * 批量创建或修改数据
+     * <br/> 如果没有数据就添加，如果数据存在就修改
+     * @param objs
+     * @return
+     * @throws SQLException
+     */
     public Dao.CreateOrUpdateStatus createOrUpdate(Collection<T> objs) throws SQLException {
         return mDao.createOrUpdate(objs);
     }
 
+    /**
+     * 在事务中批量修改数据
+     * @param objs
+     * @return
+     * @throws SQLException
+     */
     public boolean updateInTransaction(final List<T> objs) throws SQLException {
         return baseTransaction(UPDATE, objs);
     }
@@ -245,6 +354,4 @@ public abstract class OrmDaoUtils<T> {
         });
         return b;
     }
-
-
 }
